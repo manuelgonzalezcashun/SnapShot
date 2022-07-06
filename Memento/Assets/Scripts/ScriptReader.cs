80f06577ca9b62d5f30b3c2a358d051ff0152a0e
dilligak2362 <71229173+dilligak2362@users.noreply.github.com>
2022-07-06 02:10:24 -0400

    Update ScriptReader.cs

--- Memento/Assets/Scripts/ScriptReader.cs
+++ Memento/Assets/Scripts/ScriptReader.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using Ink.Runtime;
 using TMPro;
+
 public class ScriptReader : MonoBehaviour
 {
     [SerializeField] private TextAsset _InkJsonFile;
@@ -12,13 +13,20 @@ public class ScriptReader : MonoBehaviour
     public TMP_Text dialogueBox;
     public TMP_Text nameTag;
 
+    [SerializeField]
+    private GridLayoutGroup choiceHolder;
+
+    [SerializeField]
+    private Button choiceBasePrefab;
+
     void Start()
     {
         LoadStory();
     }
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             DisplayNextLine();
         }
     }
@@ -52,10 +60,58 @@ public class ScriptReader : MonoBehaviour
             dialogueBox.text = text;
             StartCoroutine(TypeSentence(text));
         }
-        else
+        else if (_StoryScript.currentChoices.Count > 0)
+        {
+            DisplayChoices();
+        }
+        else 
         {
             dialogueBox.text = "Bye";
         }
     }
 
+
+    private void DisplayChoices()
+    {
+        if (choiceHolder.GetComponentsInChildren<Button>().Length > 0) return; 
+
+        for (int i = 0; i < _StoryScript.currentChoices.Count; i++)
+        {
+            var choice = _StoryScript.currentChoices[i];
+            var button = CreateChoiceButton(choice.text);
+
+            button.onClick.AddListener(()=> OnClickChoiceButton(choice));
+        }
+    }
+
+    Button CreateChoiceButton(string text)
+    {
+
+        var choiceButton = Instantiate(choiceBasePrefab);
+        choiceButton.transform.SetParent(choiceHolder.transform, false);
+
+        var buttonText = choiceButton.GetComponentInChildren<TMP_Text>();
+        buttonText.text = text;
+
+        return choiceButton;
+    }
+
+
+    void OnClickChoiceButton(Choice choice)
+    {
+        _StoryScript.ChooseChoiceIndex(choice.index);
+        RefreshChoiceView();
+        DisplayNextLine();
+    }
+    
+    void RefreshChoiceView()
+    {
+        if (choiceHolder != null)
+        {
+            foreach(var button in choiceHolder.GetComponentsInChildren<Button>())
+            {
+                Destroy(button.gameObject);
+            }
+        }
+    }
 }
