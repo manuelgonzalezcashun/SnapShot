2e154aab335a380fc227b3bf6377cd45ed0910f3
Kylen Dilligard <kidf6m3@gmail.com>
2022-07-14 17:03:22 -0400

    Camera Mechanic

---
+++ Memento/Assets/Scripts/PhotoCapture.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotoCapture : MonoBehaviour
+{
+    [Header("Photo Taker")]
+    [SerializeField] private Image photoDisplayArea;
+    [SerializeField] private GameObject photoFrame;
+
+    [Header("Flash Effect")]
+    [SerializeField] private GameObject cameraFlash;
+    [SerializeField] private float flashTime;
+
+    private Texture2D screenCapture;
+    private bool viewingPhoto;
+
+    private void Start()
+    {
+        screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!viewingPhoto)
+            {
+                StartCoroutine(CapturePhoto());
+            }
+            else
+            {
+                RemovePhoto();
+            }
+        }
+    }
+
+    IEnumerator CapturePhoto()
+    {
+        //Camera UI set false
+        viewingPhoto = true;
+
+        yield return new WaitForEndOfFrame();
+
+        Rect regionToRead = new Rect(0, 0, Screen.width, Screen.height);
+
+        screenCapture.ReadPixels(regionToRead, 0, 0, false);
+        screenCapture.Apply();
+        ShowPhoto();
+    }
+
+    void ShowPhoto()
+    {
+        //Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100.0f);
+        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100.0f);
+        photoDisplayArea.sprite = photoSprite;
+
+        photoFrame.SetActive(true);
+        StartCoroutine(CameraFlashEffect());
+    }
+    
+    IEnumerator CameraFlashEffect()
+    {
+        //Play some audio 
+        cameraFlash.SetActive(true);
+        yield return new WaitForSeconds(flashTime);
+        cameraFlash.SetActive(false);
+    }
+
+    void RemovePhoto()
+    {
+        viewingPhoto = false;
+        photoFrame.SetActive(false);
+        //CameraUI true 
+    }
+}
