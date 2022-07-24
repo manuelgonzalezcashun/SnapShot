a908178437919ae61880762021a0f73e83d061d3
Kylen Dilligard <kidf6m3@gmail.com>
2022-07-23 22:00:14 -0400

    Non mashing button Change

--- Memento/Assets/Scripts/TextTrigger.cs
+++ Memento/Assets/Scripts/TextTrigger.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonTrigger : MonoBehaviour
+public class TextTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject textTrigger;
 
-    // Update is called once per frame
-    void Update()
+    private bool viewingText;
+
+
+    public void TextShow()
     {
-        
+        viewingText = true;
+        textTrigger.SetActive(true);
+
     }
 }
