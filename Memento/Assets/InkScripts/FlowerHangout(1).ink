5669032670d7fe53feb885528cc9650105ad84a4
Manuel Gonzalez-Cashun <manuelgonzalezcashun@gmail.com>
2022-08-03 13:35:25 -0700

    Added Flower hangout ink scripts
    Added
    =====
    Dialogue Manager
    Flower Hangout Scripts
    playAnimation Script
    
    Removed
    ======
    Old Scenes
    Dialogue Variables
    RIP Script reader

---
+++ Memento/Assets/InkScripts/FlowerHangout(1).ink
@@ -0,0 +1,13 @@
+#speaker: 
+// variables
+VAR notification = false
+-> main
+===main==
+It is 9:00 am on Saturday. 
+#speaker: StarRail 
+("Yawn. What a beautiful day outside.") 
+("What am I gonna do today?")
+"*bzzt*" "*bzzt*" #speaker: #gotText: true
+What's this, it looks like I have a text from Flower. #speaker:StarRail
+I better check it out!
+-> DONE
---
+++ Memento/Assets/Scripts/playAnimation.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playAnimation : MonoBehaviour
+{
+    private DialogueManager dm;
+    [Header("Phone")]
+    [SerializeField] private GameObject Phone;
+    private Animation PhoneAnim;
+    [Header("Background Switcher")]
+    [SerializeField] private GameObject Background;
+    private Animation bgChange;
+
+    void Start()
+    {
+        PhoneAnim = Phone.GetComponent<Animation>();
+        bgChange = Background.GetComponent<Animation>();
+    }
+
+    public void PlayPhoneAnimation(string animName)
+    {
+        PhoneAnim.Play(animName);
+    }
+    public void PlayBackgroundAnimation(string animName)
+    {
+        bgChange.Play(animName);
+    }
+}
