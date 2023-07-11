4b28f45cc1cddeec1d92ffcb6a725f30991b3449
Manuel Gonzalez Cashun <manuelgonzalezcashun@gmail.com>
2023-07-11 10:03:58 -0700

    Added the save system to the project

---
+++ SnapShot/Assets/Scripts/Save and Load System/SaveLoadSystem/FileDataHandler.cs
@@ -0,0 +1,64 @@
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FileDataHandler
+{
+    private string dataDirPath = "";
+    private string dataFileName = "";
+
+    public FileDataHandler(string dataDirPath, string dataFileName)
+    {
+        this.dataDirPath = dataDirPath;
+        this.dataFileName = dataFileName;
+    }
+    public GameData Load()
+    {
+        string fullpath = Path.Combine(dataDirPath, dataFileName);
+        GameData loadedData = null;
+        if (File.Exists(fullpath))
+        {
+            try
+            {
+                string dataToLoad = "";
+                using (FileStream stream = new FileStream(fullpath, FileMode.Open))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        dataToLoad = reader.ReadToEnd();
+                    }
+                }
+
+                loadedData = JsonUtility.FromJson<GameData>(dataToLoad);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("Error occured when trying to load data from file: " + fullpath + "\n" + e);
+            }
+        }
+        return loadedData;
+    }
+    public void Save(GameData data)
+    {
+        string fullpath = Path.Combine(dataDirPath, dataFileName);
+        try
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(fullpath));
+            string dataToStore = JsonUtility.ToJson(data, true);
+
+            using (FileStream stream = new FileStream(fullpath, FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.Write(dataToStore);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Error occured when trying to save data to file: " + fullpath + "\n" + e);
+        }
+    }
+}
