ae49fa3e664a86f9184c37ae74332ffbce4b0020
Manuel Gonzalez Cashun <manuelgonzalezcashun@gmail.com>
2023-07-18 16:27:26 -0700

    Inventory System in progress

--- SnapShot/Assets/Scripts/Inventory/Inventory.cs
+++ SnapShot/Assets/Scripts/Inventory/Inventory.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    List<GameObject> items = new List<GameObject>();
+    private List<GameObject> items = new List<GameObject>();
+    [SerializeField] private GameObject[] inventorySlots;
     private void Start()
     {
-        foreach (GameObject item in items) 
+        foreach (GameObject item in items)
         {
             Debug.Log(item.name);
         }
@@ -15,10 +17,23 @@ public class Inventory : MonoBehaviour
     public void AddInventory(GameObject obj)
     {
         items.Add(obj);
+        ShowItems();
         Destroy(obj);
+    }
+    private void ShowItems()
+    {
+        int i = 0;
         foreach (GameObject item in items)
         {
-            Debug.Log(item.name);
+            inventorySlots[i].GetComponentInChildren<Image>().sprite = item.GetComponent<SpriteRenderer>().sprite;
+            i++;
+        }
+    }
+    public void UseItem()
+    {
+        foreach (GameObject item in items)
+        {
+            Instantiate(item);
         }
     }
 }
