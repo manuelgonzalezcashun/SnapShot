2d06113b727e535ca58926231ee5e533cd97760f
Manuel Gonzalez Cashun <manuelgonzalezcashun@gmail.com>
2023-07-28 00:49:16 -0700

    Fixed Relationship Meter

--- SnapShot/Assets/Scripts/Relationship Mechanic/RelationshipMeter.cs
+++ SnapShot/Assets/Scripts/Relationship Mechanic/RelationshipMeter.cs
@@ -1,29 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RelationshipMeter : MonoBehaviour
 {
-    [field: SerializeField] public GameObject relationship_meter { get; private set; }
-    [SerializeField] GameObject[] meter_bars;
+    [SerializeField] private Slider relMeter;
     public int inkRelationshipScore { get; private set; }
     private int relationshipScore;
     private int maxRelationshipScore = 5;
     void Update()
     {
         inkRelationshipTracker();
-        RelationshipMeterFiller();
-    }
-    void RelationshipMeterFiller()
-    {
-        for (int i = 0; i < meter_bars.Length; i++)
-        {
-            meter_bars[i].SetActive(!DisplayScorePoints(relationshipScore, i));
-        }
-    }
-    bool DisplayScorePoints(int _relationshipScore, int pointNumber)
-    {
-        return pointNumber >= _relationshipScore;
+        Fill();
     }
     void inkRelationshipTracker()
     {
@@ -50,5 +39,9 @@ public class RelationshipMeter : MonoBehaviour
         #endregion
 
         relationshipScore = inkRelationshipScore;
+    }
+    public void Fill()
+    {
+        relMeter.value = relationshipScore;
     }
 }
