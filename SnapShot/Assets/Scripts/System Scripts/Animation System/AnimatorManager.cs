142f5a8526ce80862dcde04fa0e792d852dfcd3a
Manuel Gonzalez Cashun <manuelgonzalezcashun@gmail.com>
2023-08-11 18:28:33 -0700

    Fixed the animation states

--- SnapShot/Assets/Scripts/System Scripts/Animation System/AnimatorManager.cs
+++ SnapShot/Assets/Scripts/System Scripts/Animation System/AnimatorManager.cs
@@ -1,9 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.XR.Haptics;
 
 public class AnimatorManager : MonoBehaviour
 {
     [SerializeField] private List<Animations> animations;
+    private string currentState;
+
+    const string HEART_FILLING = "Heart Filling";
+    const string SCORE_UP = "Score UP";
+    const string SCORE_DOWN = "Score DOWN";
+    const string RESET_STATE = "Reset State";
     private void OnEnable()
     {
         InkDialogueObserver.onReachedMaxScore += PlayHeartAnimation;
@@ -32,15 +39,32 @@ public class AnimatorManager : MonoBehaviour
     {
         if (currentScore == 5)
         {
-            PlayAnimation("Heart Filling");
+            ChangeAnimationState(HEART_FILLING);
         }
     }
     private void PlayScoreUPAnimation()
     {
-        PlayAnimation("Score UP");
+        ChangeAnimationState(SCORE_UP);
+        Invoke("ResetState", 0.5f);
     }
     private void PlayScoreDOWNAnimation() 
     {
-        PlayAnimation("Score DOWN");
+        ChangeAnimationState(SCORE_DOWN);
+        Invoke("ResetState", 0.5f);
+
+    }
+
+    private void ResetState()
+    {
+        ChangeAnimationState(RESET_STATE);
+    }
+
+    private void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+        
+        PlayAnimation(newState);
+
+        currentState = newState;
     }
 }
